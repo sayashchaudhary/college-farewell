97278cd2f7a5e699c08637b8cf81b21d0b2f6010
Sayash Chaudhary <sayashchaudhary@Sayashs-MacBook-Air.local>
2019-12-04 22:36:09 +0530

    initial commit

--- js/home.js
+++ js/home.js
@@ -1,27 +1,27 @@
-var count = new Date("june 08,2019 18:00:00").getTime();
-var a = setInterval(function (){
-    var now = new Date().getTime();
-    var b = count - now;
+const count = new Date("june 08,2019 18:00:00").getTime();
+const a = setInterval(function () {
+    const now = new Date().getTime();
+    const b = count - now;
 
-    var days = Math.floor(b/(1000*60*60*24));
-    var hours = Math.floor(b%(1000*60*60*24)/(1000*60*60));
-    var minutes = Math.floor(b%(1000*60*60)/(1000*60));
-    var seconds = Math.floor(b%(1000*60)/1000);
+    let days = Math.floor(b / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(b % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minutes = Math.floor(b % (1000 * 60 * 60) / (1000 * 60));
+    let seconds = Math.floor(b % (1000 * 60) / 1000);
 
     document.getElementById('days').innerHTML = days;
     document.getElementById('hours').innerHTML = hours;
     document.getElementById('minutes').innerHTML = minutes;
     document.getElementById('seconds').innerHTML = seconds;
 
-    if (b <=0) {
-        var days = 0;
-        var hours = 0;
-        var minutes = 0;
-        var seconds = 0;
+    if (b <= 0) {
+        days = 0;
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
 
         document.getElementById('days').innerHTML = days;
         document.getElementById('hours').innerHTML = hours;
         document.getElementById('minutes').innerHTML = minutes;
         document.getElementById('seconds').innerHTML = seconds;
     }
-},1000);
+}, 1000);
